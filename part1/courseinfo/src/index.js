1ec4675d6a025ea4abc77679f9ba290e31c6a663
moscuet <mostifizur.rahman@integrify.iogit config --global user.email mostifizur.rahman@integrify.io>
2020-08-25 15:29:07 +0300

    assignemnt 1.3 completed

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,5 +1,6 @@
-import React from '../node_modules/@types/react'
-import ReactDOM from '../node_modules/@types/react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 
 const Header = (props) => {
   return(
@@ -11,7 +12,7 @@ const Header = (props) => {
 const Part = (props) => {
   return(
     <>
-      <p>{props.part} {props.exercise}</p>
+      <p>{props.part} {props.exercises}</p>
     </>
   )
 }
@@ -19,9 +20,9 @@ const Part = (props) => {
 const Content = (props) => {
   return(
     <div>
-        <Part exercise = {props.exercises1} part={props.part1}/>
-        <Part exercise = {props.exercises2} part={props.part2}/>
-        <Part exercise = {props.exercises3} part={props.part3}/>
+        <Part part = {props.part1.name} exercises={props.part1.exercises}/>
+        <Part part= {props.part2.name} exercises={props.part2.exercises}/>
+        <Part part = {props.part3.name} exercises={props.part3.exercises}/>
     </div>
   )
 }
@@ -36,20 +37,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Content part1={part1}  part2={part2} part3={part3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
     </div>
   )
 }
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
