53903c47580b0cf4c26968dabe207b923484dfe8
moscuet <mostifizur.rahman@integrify.iogit config --global user.email mostifizur.rahman@integrify.io>
2020-08-26 12:50:32 +0300

    1.10: unicafe step5

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,17 +6,21 @@ const Button = (props) => (
     {props.text}
   </button>
 )
+const Statistic = ({text,value}) => {
+  return(
+    <p>{text}  {value}</p>
+  )
+}
 const Statistics = ({good,bad,neutral}) =>{
   if(good+bad+neutral<=0)return <p>no  feedback given</p>
   return(
     <div>
-      <h3>Statistic</h3>
-      <p>Good  {good}</p>
-      <p>Neutral  {neutral} </p>
-      <p>Bad  {bad}</p>
-      <p>All  {good+bad+neutral}</p>
-      <p>Average  {(good-bad)/(good+bad+neutral)}</p>
-      <p>Positive  {good*100/(good+bad+neutral)}</p>
+      <Statistic text='good' value ={good}/>
+      <Statistic text='neutral' value ={neutral}/>
+      <Statistic text='bad' value ={bad}/>
+      <Statistic text='All' value ={good+bad+neutral}/>
+      <Statistic text='Average' value ={(good-bad)/(good+bad+neutral)}/>
+      <Statistic text='Positive' value ={good*100/(good+bad+neutral)}/>
    </div>
   )
 }
@@ -29,12 +33,13 @@ const App = () => {
   return (
     <div>
       <h2>Give feedback</h2>
-     <div>
-       <Button onClick={()=>setGood(good+1)} text = 'Good'/>
-       <Button onClick={()=>setNeutral(neutral+1)} text = 'Neutral'/>
-       <Button onClick={()=>setBad(bad+1)} text = 'Bad'/>
-     </div>
-        <Statistics good={good} bad={bad} neutral ={neutral} />
+      <div>
+        <Button onClick={()=>setGood(good+1)} text = 'Good'/>
+        <Button onClick={()=>setNeutral(neutral+1)} text = 'Neutral'/>
+        <Button onClick={()=>setBad(bad+1)} text = 'Bad'/>
+      </div>
+      <h3>Statistic</h3>
+      <Statistics good={good} bad={bad} neutral ={neutral} />
     </div>
   )
 }
