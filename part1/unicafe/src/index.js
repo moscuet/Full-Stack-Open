07137aeac778d1ea2bf437377701435159158d4b
moscuet <mostifizur.rahman@integrify.iogit config --global user.email mostifizur.rahman@integrify.io>
2020-08-26 12:07:18 +0300

    1.8: unicafe step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,9 +6,21 @@ const Button = (props) => (
     {props.text}
   </button>
 )
+const Statistics = ({good,bad,neutral}) =>{
+  return(
+    <div>
+      <h3>Statistic</h3>
+      <p>Good  {good}</p>
+      <p>Neutral  {neutral} </p>
+      <p>Bad  {bad}</p>
+      <p>All  {good+bad+neutral}</p>
+      <p>Average  {good+bad+neutral>0?(good-bad)/(good+bad+neutral):'Not available'}</p>
+      <p>Positive  {good+bad+neutral>0?good*100/(good+bad+neutral):'Not available'}</p>
+   </div>
+  )
+}
 
 const App = () => {
-  // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -21,15 +33,7 @@ const App = () => {
        <Button onClick={()=>setNeutral(neutral+1)} text = 'Neutral'/>
        <Button onClick={()=>setBad(bad+1)} text = 'Bad'/>
      </div>
-     <div>
-       <h3>Statistic</h3>
-       <p>Good  {good}</p>
-       <p>Neutral  {neutral} </p>
-       <p>Bad  {bad}</p>
-       <p>All  {good+bad+neutral}</p>
-       <p>Average  {good+bad+neutral>0?(good-bad)/(good+bad+neutral):'Not available'}</p>
-       <p>Positive  {good+bad+neutral>0?good*100/(good+bad+neutral):'Not available'}</p>
-     </div>
+        <Statistics good={good} bad={bad} neutral ={neutral} />
     </div>
   )
 }
