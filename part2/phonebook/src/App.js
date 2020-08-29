ff196642d398b82dee1e80e308c118a53e6ca62b
moscuet <mostifizur.rahman@integrify.iogit config --global user.email mostifizur.rahman@integrify.io>
2020-08-29 22:08:02 +0300

    2.11 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 const App = () => {
 //state
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
-  const [ search, setSearch ] = useState(persons)
+  const [ search, setSearch ] = useState([])
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+        setSearch(response.data)
+      })
+  }, [])
+  
+
+
  //handle
-  const changeNameHandle = (e) =>setNewName(e.target.value)
+  const changeNameHandle = (e) =>{
+    setNewName(e.target.value)
+    document.getElementById('message').textContent = ''
+
+  }
   const changeNumberHandle = (e) =>setNewNumber(e.target.value)
   const searchHandle = (e) =>{
+    console.log(persons)
     let filteredArr = persons.filter( a => a.name.toUpperCase().includes(e.target.value.toUpperCase()))
     setSearch(filteredArr)
   }
@@ -40,13 +52,13 @@ const App = () => {
   }
 
   return (
-    <div>
+    <div style={{margin:'20px'}}>
       <h2>Phonebook</h2>
       <Filter searchHandle = {searchHandle}/>
       <h3>Add a new</h3>
       <PersonForm submitHandle ={submitHandle} changeNameHandle= {changeNameHandle} changeNumberHandle = {changeNumberHandle} newName ={newName} newNumber = {newNumber} />
       <h2>Numbers</h2>
-       <p id ='message'></p>
+       <p id ='message' style={{color:'red'}}></p>
        <Persons persons ={search}/>
     </div>
   )
